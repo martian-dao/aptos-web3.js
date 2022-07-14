ea2ba0615907a0276c46d8a83c0ec9c89adfbf97
Martian Abhishek <abhishek@martiandao.xyz>
2022-07-14 11:14:57 +0530

    bug fix and add new command npm run publish_module

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './hex_string';
 export * from './aptos_client';
 export * from './faucet_client';
 export * from './token_client';
+export * from './wallet_client';
 export * from './types';
 export * from './transaction_builder';
