4505f6c6eecd1a6ef365eaf760cf963060c5c569
Martian Abhishek <abhishek@martiandao.xyz>
2022-08-19 02:08:09 +0530

    exported wallet_client

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ export * from "./aptos_client";
 export * from "./faucet_client";
 export * from "./token_client";
 export * from "./transaction_builder";
+export * from "./wallet_client";
 export * as TokenTypes from "./token_types";
 export * as Types from "./generated/index";
