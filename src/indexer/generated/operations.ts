a6c8a9d27d978025310afac30e3b4e9df0123698
MartianAbhishek <abhishek@martiandao.xyz>
2023-06-26 23:59:30 +0530

    chore: synced with latest aptos sdk

--- src/indexer/generated/operations.ts
+++ src/indexer/generated/operations.ts
@@ -342,25 +342,32 @@ export type GetTokenActivitiesCountQuery = {
 };
 
 export type GetTokenDataQueryVariables = Types.Exact<{
-  token_id?: Types.InputMaybe<Types.Scalars["String"]>;
+  where_condition?: Types.InputMaybe<Types.Current_Token_Datas_V2_Bool_Exp>;
 }>;
 
 export type GetTokenDataQuery = {
   __typename?: "query_root";
-  current_token_datas: Array<{
-    __typename?: "current_token_datas";
-    token_data_id_hash: string;
-    name: string;
-    collection_name: string;
-    creator_address: string;
-    default_properties: any;
-    largest_property_version: any;
-    maximum: any;
-    metadata_uri: string;
-    payee_address: string;
-    royalty_points_denominator: any;
-    royalty_points_numerator: any;
+  current_token_datas_v2: Array<{
+    __typename?: "current_token_datas_v2";
+    token_data_id: string;
+    token_name: string;
+    token_uri: string;
+    token_properties: any;
+    token_standard: string;
+    largest_property_version_v1?: any | null;
+    maximum?: any | null;
+    is_fungible_v2?: boolean | null;
     supply: any;
+    last_transaction_version: any;
+    last_transaction_timestamp: any;
+    current_collection?: {
+      __typename?: "current_collections_v2";
+      collection_id: string;
+      collection_name: string;
+      creator_address: string;
+      uri: string;
+      current_supply: any;
+    } | null;
   }>;
 };
 
